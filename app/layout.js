49d093778feb1d12c081cf61d4caf2bbbe0f72f5
Niraj0051 <nirajpingale902894@gmail.com>
2025-11-22 23:05:29 +0530

    changes the icon

--- app/layout.js
+++ app/layout.js
@@ -3,6 +3,9 @@ import "./globals.css";
 export const metadata = {
   title: "Expense Tracker",
   description: "Track your expenses with ease",
+  icons: {
+    icon: "/expense.png",
+  },
 };
 
 export default function RootLayout({ children }) {
